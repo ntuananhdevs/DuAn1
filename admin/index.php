4953a53a67a114a4ae4551fd3b3b647fd7965f93
Hman-0 <duymanh2005.ndm@gmail.com>
2024-10-29 19:52:20 +0700

    Your commit message describing the changes

--- admin/index.php
+++ admin/index.php
@@ -3,12 +3,13 @@ require_once '../commons/env.php';
 require_once '../commons/core.php';
 
 require_once '../admin/controllers/HomeController.php';
+require_once '../admin/controllers/Comment.php';
+require_once '../admin/models/Comment.php';
 
 // require_once '../admin/models/products.php';
 
 $home = new HomeController();
-
-
+$comment = new CommentController();
 // Route
 $act = $_GET['act'] ?? '/';
 
@@ -18,8 +19,10 @@ if ($act == 'login') {
     // $auth->check_login();
     include '../admin/views/layout/header.php';
 
+
     match ($act) {
         '/' => $home->views_home(),
+        'comments' => $comment->views_comment(),
 
     };
 
