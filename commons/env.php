e65a36ce03c24f16451df83a722884a24b8c2030
ntuananhdevs <anhndt25@gmail.com>
2024-10-29 14:33:04 +0700

    fix connect db

--- commons/env.php
+++ commons/env.php
@@ -4,7 +4,7 @@ define('BASE_URL'       , 'http://localhost:8008');
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Nobita@2001');
-define('DB_NAME'    , '');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'DuAn1');
 
 define('PATH_ROOT'    , __DIR__ . '/../');
