cd80165b30414f22a605525db50b999f412d3bf8
BaHien39205 <bahienphongxa@gmail.com>
2024-11-04 22:55:45 +0700

    file sua xoa category

--- admin/controllers/CategoryController.php
+++ admin/controllers/CategoryController.php
@@ -4,12 +4,40 @@ require_once 'models/Category.php';
 class CategoryController {
     public $categoryModel;
 
-    public function __construct(){
+    public function __construct() {
         $this->categoryModel = new Category();
     }
 
     public function index() {
         $listCategories = $this->categoryModel->get_categories();
         require_once './views/Category/category.php';
+    }
+
+    public function addCategory() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $category_name = $_POST['category_name'];
+            $description = $_POST['description'];
+            $this->categoryModel->add($category_name, $description);
+            header("Location: index.php?act=category");
+        } else {
+            require_once './views/Category/add_category.php';
+        }
+    }
+
+    public function editCategory($id) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $category_name = $_POST['category_name'];
+            $description = $_POST['description'];
+            $this->categoryModel->update($id, $category_name, $description);
+            header("Location: index.php?act=category");
+        } else {
+            $category = $this->categoryModel->categorybyid($id);
+            require_once './views/Category/edit_category.php';
+        }
+    }
+
+    public function deleteCategory($id) {
+        $this->categoryModel->delete($id);
+        header("Location: index.php?act=category");
     }
 }
