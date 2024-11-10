6da7dff80c585b48710b4ed78c41ab7d6704306c
ntuananhdevs <anhndt25@gmail.com>
2024-11-10 14:36:16 +0700

    change logo2

--- admin/views/layout/header.php
+++ admin/views/layout/header.php
@@ -8,7 +8,7 @@ ob_start();
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/logo2.png">
+  <link rel="icon" type="image/png" href="../assets/img/logo.png">
   <title>
     Admin
   </title>
