039152896da19a461f0c7ab3f76d6fa8766ed80b
RacheLmix <tungxeko912@gmail.com>
2024-11-24 20:47:31 +0700

    Rachel

--- clients/controllers/AuthController.php
+++ clients/controllers/AuthController.php
@@ -7,26 +7,35 @@ class AuthController {
     }
 
     public function register() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $user_name = $_POST['user_name'] ?? null;
-            $fullname = $_POST['fullname'] ?? null;
-            $phone_number = $_POST['phone_number'] ?? null;
-            $email = $_POST['email'] ?? null;
-            $password = $_POST['password'] ?? null;
+        try {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $user_name = $_POST['user_name'] ?? null;
+                $fullname = $_POST['fullname'] ?? null;
+                $phone_number = $_POST['phone_number'] ?? null;
+                $email = $_POST['email'] ?? null;
+                $password = $_POST['password'] ?? null;
     
-            if ($user_name && $fullname && $email && $password && $phone_number) {
-                if ($this->authModel->register($user_name, $fullname, $email, $password, $phone_number)) {
-                    header('Location: index.php?act=login');
-                    exit;
+                // Kiểm tra các trường thông tin
+                if ($user_name && $fullname && $email && $password && $phone_number) {
+                    // Thử đăng ký người dùng
+                    if ($this->authModel->register($user_name, $fullname, $email, $password, $phone_number)) {
+                        header('Location: index.php?act=login');
+                        exit;
+                    } else {
+                        throw new Exception("Đăng ký không thành công. Vui lòng thử lại.");
+                    }
                 } else {
-                    echo "Đăng ký không thành công. Vui lòng thử lại.";
+                    throw new Exception("Vui lòng điền đầy đủ thông tin.");
                 }
-            } else {
-                echo "Vui lòng điền đầy đủ thông tin.";
             }
+        } catch (Exception $e) {
+            
         }
+    
+        // Bao gồm giao diện
         include './clients/auth/AuthLogin.php';
     }
+    
     
 
     public function login() {
