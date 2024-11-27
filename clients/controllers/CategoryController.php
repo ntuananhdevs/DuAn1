e4ba3385c93517b1acdec73e87188ec6a741408e
RacheLmix <tungxeko912@gmail.com>
2024-11-27 14:10:22 +0700

    Rachel

--- clients/controllers/CategoryController.php
+++ clients/controllers/CategoryController.php
@@ -1,15 +1,5 @@
 <?php
 class CategoryController{
-    public $conn;
-
-    public function __construct() {
-        $this->conn = connectDB();
-    }
-
-    public function getAllCategories(){
-        $sql = "SELECT * FROM category";
-        $result = $this->conn->query($sql);
-        return $result;
-    }
+    
 }
 ?>
